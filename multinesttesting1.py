ad3f469b5507239710290e84c3a50c84d9c7a4b8
binaryfunt <binaryfunt@users.noreply.github.com>
2017-02-14 17:23:18 +0000

    Improve readibility

--- multinesttesting1.py
+++ multinesttesting1.py
@@ -5,55 +5,55 @@ import pymultinest
 import json
 
 def Gaussian_2D(coords, centre, width, height):
-    x0=centre[0]
-    y0=centre[1]
-    w0=width[0]
-    w1=width[1]
-    
-    norm=1.
-    
+    x0 = centre[0]
+    y0 = centre[1]
+    w0 = width[0]
+    w1 = width[1]
+
+    norm = 1.
+
     for i in range(len(width)):
-        norm*=1./(width[i]*(2*np.pi)**0.5)
-    
-    result_gaussian=height*norm*np.exp(-0.5*(((coords[0]-x0)/w0)**2 + ((coords[1]-y0)/w1)**2))
-    
+        norm *= 1. / (width[i] * (2 * np.pi)**0.5)
+
+    result_gaussian = height * norm * np.exp(-0.5 * ( ((coords[0] - x0) / w0)**2 + ((coords[1] - y0) / w1)**2) )
+
     return result_gaussian
 
-array_size=101
+array_size = 101
 
-x=np.linspace(-5.,5.,array_size)
-y=x
-xx,yy=np.meshgrid(x,y)
+x = np.linspace(-5., 5., array_size)
+y = x
+xx, yy = np.meshgrid(x, y)
 
-num=8
-centre_list=np.random.uniform(-5.,5.,(num,2))
-width_list=np.random.uniform(0.9,1.1,(num,2))
-height_list=np.random.uniform(0.,2.,num)
+num = 8
+centre_list = np.random.uniform(-5., 5., (num,2))
+width_list = np.random.uniform(0.9, 1.1, (num,2))
+height_list = np.random.uniform(0., 2., num)
 #widths=np.ones((num,2))
 
 print centre_list
 print width_list
 
-data=np.zeros((array_size,array_size))
+data = np.zeros((array_size, array_size))
 
 for i in range(num):
-    data+=Gaussian_2D(np.array([xx,yy]), centre_list[i], width_list[i], height_list[i])
+    data += Gaussian_2D(np.array([xx,yy]), centre_list[i], width_list[i], height_list[i])
 
 plt.pcolormesh(x,y,data)
 
 #Multinest time
 
 def Model(number, centres, widths, heights):
-    result=np.zeros((array_size,array_size))
-    
+    result = np.zeros((array_size,array_size))
+
     for i in range(number):
-        result+=Gaussian_2D(np.array([xx,yy]), centre_list[i], width_list[i], height_list[i])
-    
+        result += Gaussian_2D(np.array([xx,yy]), centre_list[i], width_list[i], height_list[i])
+
     return result
 
 def Prior(cube, ndim, nparams):
     for i in range(ndim):
-        cube[i]=cube[i]*10. - 5.
+        cube[i] = cube[i]*10. - 5.
 
 def Loglike(cube, ndim, nparams):
         '''
@@ -63,15 +63,15 @@ def Loglike(cube, ndim, nparams):
             2=widths array of 2d arrays
             3=heights array of floats
         '''
-        model=np.log(Model(cube[0], cube[1], cube[2], cube[3]))
-        loglikelihood=(-0.5 * ((model - data))**2).sum()
+        model = np.log(Model(cube[0], cube[1], cube[2], cube[3]))
+        loglikelihood = (-0.5 * ((model - data))**2).sum()
         return loglikelihood
 
-parameters=["number", "centres", "widths", "heights"]
-n_params=len(parameters)
+parameters = ["number", "centres", "widths", "heights"]
+n_params = len(parameters)
 
 # run MultiNest
-pymultinest.run(Loglike, Prior, n_params)
-json.dump(parameters) # save parameter names
+# pymultinest.run(Loglike, Prior, n_params)
+# json.dump(parameters) # save parameter names
 
 plt.show()
