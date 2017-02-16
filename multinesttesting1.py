e62ba5dbd7617ac0088165cee44cd7cb7d985d93
tristan9543 <tristanlaurens@hotmail.com>
2017-02-16 16:39:04 +0000

    Tidying up

--- multinesttesting1.py
+++ multinesttesting1.py
@@ -10,44 +10,43 @@ def Gaussian_2D(coords, centre, width, height):
     for i in range(len(width)):
         norm*=1./(width[i]*(2*np.pi)**0.5)
     
-    result_gaussian=height*norm*np.exp(-0.5*(((coords[0]-centre[0])/width[0])**2 + ((coords[1]-centre[1])/width[1])**2))
+    result_gaussian = height*norm*np.exp(-0.5*(((coords[0] - centre[0])/width[0])**2 + ((coords[1] - centre[1])/width[1])**2))
     
     return result_gaussian
 
-array_size=1001
+array_size=101
 
-x=np.linspace(-5.,5.,array_size)
-y=x
-xx,yy=np.meshgrid(x,y)
+x = np.linspace(-5., 5., array_size)
+y = x
+xx, yy = np.meshgrid(x, y)
 
-num=8
-centre_list=np.random.uniform(-5.,5.,(num,2))
-width_list=np.random.uniform(0.9,1.1,(num,2))
-height_list=np.random.uniform(0.,1.,num)
-#widths=np.ones((num,2))
+num=1
+centre_list = np.random.uniform(-5., 5., (num, 2))
+width_list = np.random.uniform(0.9, 1.1, (num, 2))
+height_list = np.random.uniform(0., 1., num)
 
-print centre_list
-print width_list
+print "centres", centre_list
+print "widths", width_list
+print "heights", height_list
 
 data=np.zeros((array_size,array_size))
 
 for i in range(num):
     data+=Gaussian_2D(np.array([xx,yy]), centre_list[i], width_list[i], height_list[i])
 
-plt.pcolormesh(x,y,data)
-
+plt.pcolormesh(x, y, data)
 
 
 #Multinest time
 
 def Model(number, centres, widths, heights):
     '''
-    Sum of [number] of gaussians.
+    Sum of [number] of gaussians - from Gaussian_2D function.
     '''
-    result=np.zeros((array_size,array_size))
+    result = np.zeros((array_size, array_size))
     
     for i in range(number):
-        result+=Gaussian_2D(np.array([xx,yy]), centre_list[i], width_list[i], height_list[i])
+        result += Gaussian_2D(np.array([xx, yy]), centre_list[i], width_list[i], height_list[i])
     
     return result
 
@@ -65,9 +64,9 @@ def Loglike(cube, ndim, nparams):
     '''
     Log likelihood function.
     '''
-    centres=np.zeros((number,2))
-    widths=np.zeros((number,2))
-    heights=np.zeros(number)
+    centres = np.zeros((number, 2))
+    widths = np.zeros((number, 2))
+    heights = np.zeros(number)
         
     for i in range(number):
         centres[i][0] = cube[2*i]
@@ -76,7 +75,11 @@ def Loglike(cube, ndim, nparams):
         widths[i][1] = cube[2*i+1 + 2*number]
         heights[i] = cube[i + 4*number]    
         
-    loglikelihood=np.log(Model(number, centres, widths, heights)).sum()   #not sure about this line
+    #loglikelihood=np.log(Model(number, centres, widths, heights)).sum()   #not sure about this line
+    
+    model=Model(number, centres, widths, heights)
+    loglikelihood = (-0.5 * ((model - data)/0.001)**2).sum()
+    
     return loglikelihood
 
 def Create_parameter_list(empty_list=None):
@@ -88,16 +91,16 @@ def Create_parameter_list(empty_list=None):
 
     for i in range(2*number):
         if i%2 == 0:
-            xy="x"
+            xy = "x"
         else:
-             xy="y"
+             xy = "y"
         empty_list.append("centre_" + xy + str(int(i/2)))
     
     for i in range(2*number):
         if i%2 == 0:
-            xy="x"
+            xy = "x"
         else:
-             xy="y"
+             xy = "y"
         empty_list.append("width_" + xy + str(int(i/2)))
         
     for i in range(number):
@@ -105,12 +108,12 @@ def Create_parameter_list(empty_list=None):
     
     return empty_list
 
-number = 5 #number of gaussians
+number = 1 #number of gaussians
 parameters = Create_parameter_list()
 n_params = len(parameters)
 
 # run MultiNest
-pymultinest.run(Loglike, Prior, n_params)
+pymultinest.run(Loglike, Prior, n_params, n_live_points = 1000)
 with open('chains/1-parameter_list.txt', 'w') as outfile:  
     json.dump(parameters, outfile, 'w')
 
