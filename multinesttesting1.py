8066e0e0ad0ab13da0a372122c188c0e88f086bb
binaryfunt <binaryfunt@users.noreply.github.com>
2017-02-21 12:18:24 +0000

    Remove commented out bits

--- multinesttesting1.py
+++ multinesttesting1.py
@@ -31,24 +31,14 @@ x = np.linspace(*x_range, num=array_size)
 y = np.linspace(*x_range, num=array_size)
 xxyy = np.meshgrid(x, y)
 xx, yy = xxyy
-# xxyy = np.array([[xx[i,j], yy[i,j]] for i in range(0, array_size) for j in range(0, array_size)])
 
 num = 1 #8
 centre_list = np.random.uniform(*x_range, size=(num,2))
 width_list = np.random.uniform(*sigma_range, size=(num,2))
 height_list = np.random.uniform(*amplitude_range, size=num)
-#widths=np.ones((num,2))
 
-# print centre_list
-# print width_list
-
-# Model = Gaussian_2D
 noise = 0.01
-# data = np.zeros((array_size, array_size))
-# data = np.random.normal(data, noise)
-#
-# for i in range(num):
-#     data += Gaussian_2D(xx, yy, *np.concatenate([centre_list[i], width_list[i], [height_list[i]]]))
+
 
 datafile = sys.argv[1]
 data = np.loadtxt(datafile)
@@ -57,16 +47,11 @@ color_map = LinearSegmentedColormap.from_list('mycmap', ['black', 'red', 'yellow
 plt.axis('equal')
 plt.pcolormesh(x, y, data, cmap=color_map)
 
+plt.savefig(datafile + "_fig.png")
+
 #Multinest time
 number=5 #number of gaussians
 
-# def Model(number, centres, widths, heights):
-#     result = np.zeros((array_size,array_size))
-#
-#     for i in range(number):
-#         result += Gaussian_2D(np.array([xx,yy]), centre_list[i], width_list[i], height_list[i])
-#
-#     return result
 
 
 def Prior(cube, ndim, nparams):
@@ -86,8 +71,6 @@ def Prior(cube, ndim, nparams):
     # amplitude:
     cube[4] = 2.*cube[4]
 
-    # for i in range(ndim):
-    #     cube[i] = cube[i]*10. - 5.
 
 
 def Loglike(cube, ndim, nparams):
@@ -95,19 +78,6 @@ def Loglike(cube, ndim, nparams):
     xsigma, ysigma = cube[2], cube[3]
     amplitude = cube[4]
 
-    # for i in range(ndim):
-    #     if 0 <= i <= (number*2 - 1):
-    #         centres[i][0] = cube[2*i]
-    #         centres[i][1] = cube[2*i+1]
-    #     elif (number*2) <= i <= (number*2-1 + 2*number):
-    #         widths[i][0] = cube[2*i]
-    #         widths[i][1] = cube[2*i+1]
-    #     elif (number*2+3*number) <= i <= (number-1 + 3*number):
-    #         heights[i] = cube[i]
-    #     else:
-    #         print "i wrong index"
-
-    # loglikelihood = np.log(Model(number, centres, widths, heights))
 
     model = Model(x0, y0, xsigma, ysigma, amplitude)
     loglikelihood = (-0.5 * ((model - data) / noise)**2).sum()
@@ -121,4 +91,3 @@ pymultinest.run(Loglike, Prior, n_params, outputfiles_basename=datafile + '_1_',
 json.dump(parameters, open(datafile + '_1_params.json', 'w')) # save parameter names
 
 # plt.show()
-plt.savefig(datafile + "_fig.png")
