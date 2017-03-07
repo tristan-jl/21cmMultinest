1098f353c666e35dcd45f58d1c04a3415d007d82
binaryfunt <binaryfunt@users.noreply.github.com>
2017-03-07 23:21:01 +0000

    Add support for cmd line args

--- Fcoll_test/fcoll_reader.py
+++ Fcoll_test/fcoll_reader.py
@@ -1,19 +1,21 @@
 # -*- coding: utf-8 -*-
 import numpy as np
-import sys
+import sys, getopt
 import time
 
-def load_binary_data(filename, dtype=np.float32): 
-     """ 
-     We assume that the data was written with write_binary_data() (little endian). 
-     """ 
-     f = open(filename, "rb") 
-     data = f.read() 
-     f.close() 
-     _data = np.fromstring(data, dtype) 
+USAGE = 'USAGE: fcoll_reader.py -i "<file 1> <file 2>"'
+
+def load_binary_data(filename, dtype=np.float32):
+     """
+     We assume that the data was written with write_binary_data() (little endian).
+     """
+     f = open(filename, "rb")
+     data = f.read()
+     f.close()
+     _data = np.fromstring(data, dtype)
      if sys.byteorder == 'big':
        _data = _data.byteswap()
-     return _data 
+     return _data
 
 def write_binary_data(filename, data, dtype=np.float32):
      """
@@ -26,8 +28,28 @@ def write_binary_data(filename, data, dtype=np.float32):
      f.write(_data)
      f.close()
 
-data1 = load_binary_data('Fcoll_output_file1')
-data2 = load_binary_data('Fcoll_output_file2')
+files_in = ""
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "h:i:")
+except getopt.GetoptError:
+    print USAGE
+    sys.exit(2)
+# print opts, args
+for opt, arg in opts:
+    if opt in ("-h", "--h", "--help"):
+        print USAGE
+        sys.exit()
+    elif opt in ("-i"):
+        files_in = arg
+        files_in = files_in.split()
+if files_in == "" or files_in == []:
+    print USAGE
+    sys.exit()
+
+
+data1 = load_binary_data(files_in[0])
+data2 = load_binary_data(files_in[1])
 diff = data1 - data2
 
 print data1 == data2
