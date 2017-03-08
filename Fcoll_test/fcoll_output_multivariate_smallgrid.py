a82ef81b615aa82fbf38d00cc31b944794fc87be
tristan9543 <tristanlaurens@hotmail.com>
2017-03-08 18:34:13 +0000

    Added fcoll pdf

--- Fcoll_test/fcoll_output_multivariate_smallgrid.py
+++ Fcoll_test/fcoll_output_multivariate_smallgrid.py
@@ -1,6 +1,7 @@
 import numpy as np
 from numpy import linalg
 from scipy.stats import multivariate_normal
+from scipy.stats import rv_continuous
 import sys
 import time
 
@@ -30,6 +31,22 @@ def write_binary_data(filename, data, dtype=np.float32):
 def is_pos_def(x):
     return np.all(np.linalg.eigvals(x) > 0)
 
+class Fcoll_pdf(rv_continuous):
+    def _pdf(self, x):
+        '''
+            Piecewise PDF of fcoll heights
+            sum of exp and uniform dist.
+            x: random variable
+            p, q, r: params set from distribution of fcoll heights from 21cmFAST
+            end from analysis of fcoll maps
+            '''
+        end = 1. - 0.0058135
+        p = 0.183288865538
+        q = 1261.4820295
+        r = -2.39015541247
+        norm = 1./(p/(r + 1.) + q*(1. - end)) #normalisation factor
+        return np.piecewise(x, [x<end, x>=end], [lambda x: norm*p*x**r, lambda x: norm*p*x**r + q])
+
 #create grid
 grid_length = 64
 x_ = np.linspace(0., grid_length - 1., grid_length)
@@ -42,6 +59,8 @@ pos = np.stack((x, y, z), axis = -1)
 N_peaks = 1000
 centre_list = np.random.uniform(0., grid_length, (N_peaks, 3))
 height_list = np.random.uniform(0., 1., N_peaks)
+height_list_pdf = Fcoll_pdf(a=0., b=1., name='Fcoll_pdf')
+height_list = height_list_pdf.pdf(height_list)
 rand_matrix_list = np.random.uniform(0., 1., (N_peaks, 3, 3))
 data = np.zeros((int(grid_length), int(grid_length), int(grid_length)))
 
@@ -50,11 +69,10 @@ start=time.time()
 for i in xrange(N_peaks):
     print i
     cov_matrix = np.dot(rand_matrix_list[i],rand_matrix_list[i].transpose())
-    #matrix sometimes singular, ie has very small det (havent seen one equal to 0 yet)
-    data += height_list[i] * multivariate_normal.pdf(pos, centre_list[i], cov_matrix, allow_singular = True)
+    data += height_list[i] * multivariate_normal.pdf(pos, centre_list[i], [[1., 0., 0.], [0., 1., 0.],[0., 0., 1.]], allow_singular = True)
 
 end=time.time()
-print end - start
+print "time taken", end - start
 
 outputfile = write_binary_data('Fcoll_output_file', data)
 
