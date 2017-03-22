8981507fe0dcc0b97253d66301741fb09b505b9f
binaryfunt <binaryfunt@users.noreply.github.com>
2017-03-22 20:20:36 +0000

    Add support for Gaussian amplitude

--- MN_Fcoll.py
+++ MN_Fcoll.py
@@ -43,7 +43,7 @@ class MN2:
             marginals (bool): whether or not to plot the marginals
             filename (str): the filename in the out/ directory (must be specified if any of the 3 above are True)
         """
-        self.parameters = ["x0", "y0", "z0", "width"]#["x0a", "y0a", "x0b", "y0b"]#, "sigma_x", "sigma_y", "amplitude"]
+        self.parameters = ["x0", "y0", "z0", "width", "amplitude"]#["x0a", "y0a", "x0b", "y0b"]#, "sigma_x", "sigma_y", "amplitude"]
         self.n_params = len(self.parameters)
 
         self.array_size = 64
@@ -86,7 +86,7 @@ class MN2:
         return amplitude * normalisation * np.exp(-0.5 * ( ((x-x0)/sigma)**2 + ((y-y0)/sigma)**2  + ((z-z0)/sigma)**2 ))
 
 
-    def Unimodal_Model(self, x0, y0, z0, width):#, sigma_x, sigma_y, amplitude):
+    def Unimodal_Model(self, x0, y0, z0, width, amp):#, sigma_x, sigma_y, amplitude):
         """
         Args:
             x0 (float): x coord of centre of Gaussian
@@ -94,11 +94,11 @@ class MN2:
             z0 (float): z coord of centre of Gaussian
             width (float): width of Gaussian (i.e. sigma)
         """
-        amp = 1.0
+        # amp = 1.0
         return self.Gaussian_3D(self.xyz, x0, y0, z0, width, amp)
 
 
-    def Multimodal_Model(self, x0, y0, z0, width):#, sigma_x, sigma_y, amplitude):
+    def Multimodal_Model(self, x0, y0, z0, width, amp):#, sigma_x, sigma_y, amplitude):
         """
         Args:
             x0 (array): list of x coords of centre of Gaussians
@@ -106,10 +106,10 @@ class MN2:
             z0 (array): list of z coords of centre of Gaussians
             width (array): widths of Gaussians (i.e. sigma)
         """
-        amp = 1.
+        # amp = 1.
         model = np.zeros_like(self.xyz[0])
         for i in range(len(x0)):
-            model += self.Gaussian_3D(self.xyz, x0[i], y0[i], z0[i], width[i], amp)
+            model += self.Gaussian_3D(self.xyz, x0[i], y0[i], z0[i], width[i], amp[i])
         return model
 
 
@@ -125,19 +125,22 @@ class MN2:
         def transform_centres(i):
             cube[i] = max(self.x_range) * cube[i]
         def transform_widths(i):
-            cube[i] = max(self.x_range)/10. * cube[i]
+            cube[i] = 4. * cube[i]
         # x0, y0, z0:
         for i in [0, 1, 2]:
             transform_centres(i)
         # width:
         transform_widths(3)
+        # amplitude:
+        cube[4] = cube[4]
 
 
     def Loglike(self, cube, ndim, nparams):
         x0, y0, z0 = cube[0], cube[1], cube[2]
         width = cube[3]
+        amp = cube[4]
 
-        model = self.Unimodal_Model(x0, y0, z0, width)#, x0b, y0b)#, xsigma, ysigma, amplitude)
+        model = self.Unimodal_Model(x0, y0, z0, width, amp)#, x0b, y0b)#, xsigma, ysigma, amplitude)
         loglikelihood = (-0.5 * ((model - self.data) / self.scatter)**2).sum()
 
         return loglikelihood
@@ -169,8 +172,9 @@ class MN2:
             filename (str)
             noise (float)
         """
-        x0, y0, z0 = np.random.uniform(*self.x_range, size=self.num), np.random.uniform(*self.x_range, size=self.num), np.array([32,32,32,32])#np.random.uniform(31., 33., size=self.num)
-        width = np.random.uniform(1., 5., self.num)
+        x0, y0, z0 = np.random.uniform(*self.x_range, size=self.num), np.random.uniform(*self.x_range, size=self.num), np.random.uniform(*self.x_range, size=self.num)
+        width = np.random.uniform(0.5, 3., self.num)
+        amp = np.random.uniform(0.1, 1., self.num)
 
         self.data = self.Multimodal_Model(x0, y0, z0, width)#, sigma_x, sigma_y, amplitude)
         # data = np.random.normal(data, noise)
@@ -179,7 +183,7 @@ class MN2:
         self._plot(self.data)
         plt.savefig("out/" + filename + "_fig.png")
 
-        print x0, "\n", y0, "\n", z0, "\n", width
+        print x0, "\n", y0, "\n", z0, "\n", width, "\n", amp
 
 
     def run_sampling(self, datafile, marginals=True, n_points=1000, scatter=4., resume=False, mode_tolerance=-1e90, verbose=False, max_iter=0):
@@ -209,7 +213,7 @@ class MN2:
             print "No modes detected"
             return 0
 
-        fit_data = self.Multimodal_Model(means[:,0], means[:,1], means[:,2], means[:,3])#, means[2], means[3])
+        fit_data = self.Multimodal_Model(means[:,0], means[:,1], means[:,2], means[:,3], means[:,4])
         self._plot(fit_data)
         plt.savefig(datafile + "_1_fig.png")
 
